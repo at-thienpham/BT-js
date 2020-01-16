5e1687ee2bebf1655e3224be5eaddd69df6803c0
thienpham <thien.pham@asiantech.vn>
2020-01-16 09:37:06 +0700

    update exercise js 20200116

--- Cart.js
+++ Cart.js
@@ -1,39 +1,84 @@
 var getCartValue = [];
-getCartValue = JSON.parse(localStorage.getItem('add-cart'));
-
+getCartValue = JSON.parse(localStorage.getItem('productvalue'));
+var cart = JSON.parse(localStorage.getItem('productvalue'));
 var table = document.getElementById('tablecart');
-var properties = ['id', 'name', 'quantily', 'price', 'subtotal', 'btndel'];
+var properties = ['id', 'name', 'quantily', 'price', 'count', 'subtotal', 'btndel'];
+var sum = 0;
 
 function displayTable() {
   for (var i = 0; i < getCartValue.length; i++) {
     var product = getCartValue[i];
-    var tr = document.createElement('tr');  
+
+    var tr = document.createElement('tr');
     var id = document.createElement('td');
     var name = document.createElement('td');
     var quantily = document.createElement('td');
     var price = document.createElement('td');
     var subtotal = document.createElement('td');
-    var btnadd = document.createElement('div'); 
+    var btnadd = document.createElement('td');
+    //var sum = document.createElement('td');
+    if (parseInt(product[properties[4]]) !== 0) {
+      id.innerHTML = product[properties[0]];
+      name.innerHTML = product[properties[1]];
+      quantily.innerHTML = product[properties[4]];
+      price.innerHTML = product[properties[3]];
+      subtotal.innerHTML = parseInt(product[properties[3]]) * parseInt(product[properties[4]]);
+      btnadd.innerHTML = ('<button data-id="' + product[properties[0]] + '" class="btn-delete" >X</button>');
+      sum += parseInt(product[properties[3]]) * parseInt(product[properties[4]]);
+      //sum.innerHTML = ();
+      tr.appendChild(id);
+      tr.appendChild(name);
+      tr.appendChild(quantily);
+      tr.appendChild(price);
+      tr.appendChild(subtotal);
+      tr.appendChild(btnadd);
+      //tr.appendChild(sum);
+      table.appendChild(tr);
 
-    id.innerHTML = product[properties[0]];
-    name.innerHTML = product[properties[1]];
-    quantily.innerHTML = ('<input id="tmp" type="number" value="1" min="1" max="10">');
-    price.innerHTML = product[properties[3]];
-    subtotal.innerHTML = ('<p>'+product[properties[3]]+'</p>');
-    btnadd.innerHTML = ('<button id="'+product[properties[0]]+'" onclick="deleteCart('+product[properties[0]]+')"><img src="images/delete.png"></button>');
+    }
 
-    tr.appendChild(id);
-    tr.appendChild(name);
-    tr.appendChild(quantily);
-    tr.appendChild(price);
-    tr.appendChild(subtotal);
-    tr.appendChild(btnadd);
-
-    table.appendChild(tr);
   }
 }
-displayTable();
 
-function deleteCart(x){
-  localStorage.removeItem(x);
-}
+displayTable();
+console.log(sum);
+deleteCart();
+var total_price = '<tr><td colspan = "3"></td> <td> Total </td> <td class = "total-price">' + sum + ' </td><td></td></tr>';
+document.getElementById("tablecart").innerHTML += total_price;
+
+/*function deleteCart(){
+  var btnClick = document.getElementsByClassName('btn-delete');
+  var count = 0;
+  //var cart = JSON.parse(localStorage.getItem('productvalue'));
+  for (var i = 0; i < btnClick.length; i++) {
+    var indexBtn = btnClick[i];
+    indexBtn.addEventListener('click', function (event) {
+      console.log(event.target.dataset.id);
+      var id = event.target.dataset.id;
+      
+            cart[i]['count']=0;
+            
+      localStorage.setItem('productvalue', JSON.stringify(cart));
+
+    });
+  }
+}*/
+function deleteCart() {
+  var btnClick = document.getElementsByClassName('btn-delete');
+  var count = 1;
+  //var cart = JSON.parse(localStorage.getItem('productvalue')
+
+  var product = localStorage.getItem('productvalue');
+  product = product ? JSON.parse(product) : [];
+  for (var i = 0; i < product.length; i++) {
+    for (var j = 0; j < btnClick.length; j++) {
+      var indexBtn = btnClick[j];
+      indexBtn.addEventListener('click', function(event) {
+          return product[i].count = 0;
+        });
+
+      }
+
+    }
+    
+  }
--- Product.js
+++ Product.js
@@ -1,67 +1,76 @@
-var products = [{
-  id: '1',
-  name: 'San pham 1',
-  describe: 'Mo ta san pham 1',
-  img: 'images/1.png',
-  price: '&dollar;100000',
-},
-{
-  id: '2',
-  name: 'San pham 2',
-  describe: 'Mo ta san pham 2',
-  img: 'images/2.png',
-  price: '&dollar;200000',
-},
-{
-  id: '3',
-  name: 'San pham 3',
-  describe: 'Mo ta san pham 3',
-  img: 'images/3.png',
-  price: '&dollar;300000',
-},
-{
-  id: '4',
-  name: 'San pham 4',
-  describe: 'Mo ta san pham 4',
-  img: 'images/4.png',
-  price: '&dollar;400000',
-},
-{
-  id: '5',
-  name: 'San pham 5',
-  describe: 'Mo ta san pham 5',
-  img: 'images/5.png',
-  price: '&dollar;500000',
-},
-{
-  id: '6',
-  name: 'San pham 6',
-  describe: 'Mo ta san pham 6',
-  img: 'images/6.png',
-  price: '&dollar;600000',
-},
-{
-  id: '7',
-  name: 'San pham 7',
-  describe: 'Mo ta san pham 7',
-  img: 'images/7.png',
-  price: '&dollar;700000',
-},
-{
-  id: '8',
-  name: 'San pham 8',
-  describe: 'Mo ta san pham 1',
-  img: 'images/8.png',
-  price: '&dollar;800000',
-}
+var products = [
+  {
+    id: '1',
+    name: 'product 1',
+    describe: 'describe 1',
+    img: 'images/1.png',
+    price: '100000',
+    count:0
+  },
+  {
+    id: '2',
+    name: 'product 2',
+    describe: 'describe 2',
+    img: 'images/2.png',
+    price: '200000',
+    count:0
+  },
+  {
+    id: '3',
+    name: 'product 3',
+    describe: 'describe 3',
+    img: 'images/3.png',
+    price: '300000',
+    count:0
+  },
+  {
+    id: '4',
+    name: 'product 4',
+    describe: 'describe 4',
+    img: 'images/4.png',
+    price: '400000',
+    count:0
+  },
+  {
+    id: '5',
+    name: 'product 5',
+    describe: 'describe 5',
+    img: 'images/5.png',
+    price: '500000',
+    count:0
+  },
+  {
+    id: '6',
+    name: 'product 6',
+    describe: 'describe 6',
+    img: 'images/6.png',
+    price: '600000',
+    count:0
+  },
+  {
+    id: '7',
+    name: 'product 7',
+    describe: 'describe 7',
+    img: 'images/7.png',
+    price: '700000',
+    count:0
+  },
+  {
+    id: '8',
+    name: 'product 8',
+    describe: 'describe 1',
+    img: 'images/8.png',
+    price: '800000',
+    count:0
+  }
 ];
-
 localStorage.setItem("productvalue", JSON.stringify(products));
 var item = [];
 item = JSON.parse(localStorage.getItem('productvalue'));
+var cart = JSON.parse(localStorage.getItem('productvalue'));
 
 var ul = document.getElementById('listproduct');
-var properties = ['id', 'img', 'name', 'describe', 'price'];
+var properties = ['id', 'img', 'name', 'describe', 'price','count'];
 
 function displayTable() {
   for (var i = 0; i < item.length; i++) {
@@ -72,25 +81,30 @@ function displayTable() {
     var describe = document.createElement('p');
     var price = document.createElement('p');
     var btnadd = document.createElement('div');
+    var count = document.createElement('p');
     image.innerHTML = ('<img src=" ' + product[properties[1]] + ' ">');
     name.innerHTML = product[properties[2]];
     describe.innerHTML = product[properties[3]];
     price.innerHTML = product[properties[4]];
-    btnadd.innerHTML = ('<button id="'+product[properties[0]]+'" onclick="addCart('+product[properties[0]]+')" class="btn-primary">Add to cart</button>');
-
+    btnadd.innerHTML = ('<button data-id="'+product[properties[0]]+'"  class="btn-primary">Add to cart</button>');
+    count.innerHTML = cart[i]['count'];
     row.appendChild(image);
     row.appendChild(name);
     row.appendChild(describe);
     row.appendChild(price);
     row.appendChild(btnadd);
+    row.appendChild(count);
     ul.appendChild(row);
   }
 }
-displayTable();
 
-var count = 0;
-var addToCart = [];
-function addCart(x) {
+displayTable();
+addCart();
+
+
+/*var count = 0;
+var addToCart = [];*/
+/*function addCart(x) {
   count += 1;
   document.getElementById("count").innerHTML = count;
   
@@ -98,12 +112,41 @@ function addCart(x) {
   localStorage.setItem('add-cart', JSON.stringify(addToCart));
   var getAddCart= [];
   getAddCart = JSON.parse(localStorage.getItem('add-cart'));
-}
+}*/
+function addCart(){
+  var btnClick = document.getElementsByClassName('btn-primary');
+  var count = 1;
+  //var cart = JSON.parse(localStorage.getItem('productvalue'));
+  for (var i = 0; i < btnClick.length; i++) {
+    var indexBtn = btnClick[i];
+    indexBtn.addEventListener('click', function (event) {
+      console.log(event.target.dataset.id);
+      var id = event.target.dataset.id;
+      if (cart) {
+        for (var i = 0; i < cart.length; i++) {
+          if (id === cart[i]['id']) {
+            cart[i]['count']++;
+            break;
+          }
+          else if (i === (cart.length - 1)) {
+            cart.push({  count: count });
+            break;
+          }
+        }
+      } else {
+        cart = [];
+        cart.push({  count: count });
+      }
+      localStorage.setItem('productvalue', JSON.stringify(cart));
 
-var btnClick = document.getElementsByClassName('btn-primary');
+    });
+  }
+
+}
+/*var btnClick = document.getElementsByClassName('btn-primary');
 for (var i = 0; i < btnClick.length; i++) {
   var indexBtn = btnClick[i];
   indexBtn.addEventListener('click', function() {
     event.target.setAttribute('disabled', 'disabled');
   });
-}
+}*/
